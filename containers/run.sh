583bb2ee7f18a71e239952e03bffd0e57708c6c1
konradhalas <halas.konrad@gmail.com>
2017-12-28 22:55:57 +0100

    Use socket (WSGI) instead of HTTP

--- containers/run.sh
+++ containers/run.sh
@@ -1,4 +1,4 @@
 ../containers/wait-for-it.sh -t 60 db:5432
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-uwsgi --http :8000 --module pywaw.wsgi --processes 4 --threads 2
+uwsgi --socket :8000 --module pywaw.wsgi --processes 4 --threads 2
